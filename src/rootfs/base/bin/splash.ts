6bb914d82dbb1824513608324cac4a529af49233
Nicholas-py <nicholas.waslander@gmail.com>
2026-07-10 21:58:38 -0400

    Configurable splash speeds

--- src/rootfs/base/bin/splash.ts
+++ src/rootfs/base/bin/splash.ts
@@ -9,16 +9,23 @@ export class PRSplash extends DSProcess {
         const optparser = new DSOptionParser(
             this.procname,
             true,
-            "   print the depsys splash screen"
+            "   print the depsys splash screen",
+            "<speed>"
         );
-        optparser.parseWithUsageAndHelp(this.argv);
+
+        let nextarg = optparser.parseWithUsageAndHelp(this.argv);
+
+        var baud = 2000;
+        if (nextarg != -1) {
+            baud = parseFloat(this.argv[nextarg])
+        }
 
         const inode = this.cwd.getfile("/etc/splashtext.txt")
         const text = await inode.contentAsText().read();
 
         await DSKernel.terminal.baudWrite("");
         DSKernel.terminal.reset();
-        DSKernel.terminal.baud = 2000;
+        DSKernel.terminal.baud = baud;
         await DSKernel.terminal.baudWrite(text);
         DSKernel.terminal.baud = 0;
 
